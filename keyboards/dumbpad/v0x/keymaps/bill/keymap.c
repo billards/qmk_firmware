971d1c4d83acf3880eed4106b5f8cb408378d6d3
bill.kalominidis <billkalominidis@gmail.com>
2021-09-21 19:56:44 +1000

    Add tap dance back in lol

--- keyboards/dumbpad/v0x/keymaps/bill/keymap.c
+++ keyboards/dumbpad/v0x/keymaps/bill/keymap.c
@@ -14,11 +14,18 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+enum {
+    TD_SLASH_CALC
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_SLASH_CALC] = ACTION_TAP_DANCE_DOUBLE(KC_KP_SLASH, KC_CALCULATOR)
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	/*		NUMPAD LAYER
     /-----------------------------------------------------`
-    |             |    7    |    8    |    9    |    /    |
+    |             |    7    |    8    |    9    |  / CALC |
     |             |---------|---------|---------|---------|
     |             |    4    |    5    |    6    |    *    |
     |             |---------|---------|---------|---------|
@@ -28,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     \-----------------------------------------------------'
 	*/
     [0] = LAYOUT(
-					KC_7,	KC_8,	KC_9,   	KC_KP_SLASH,
+					KC_7,	KC_8,	KC_9,   	TD(TD_SLASH_CALC),
 					KC_4,	KC_5,	KC_6,   	KC_KP_ASTERISK,
 					KC_1,	KC_2,	KC_3,		KC_KP_MINUS,
 	LT(2, KC_EQL),	KC_0,	KC_DOT,	KC_BSPACE,	KC_KP_PLUS
